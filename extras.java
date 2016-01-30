7e9657789011f5d2b2a415ad042732680ce08d0d
brendanmanning <BrendanManning19380@gmail.com>
2016-01-30 13:28:20 -0500

    Create extras.java
    extras.java is really just the code for the echo command. It was originally going to be for several commands, but it ended up being best for just echo

---
+++ extras.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class extras here.
+ * 
+ * @author (Brendan Manning) 
+ * @version (0.0.1)
+ */
+public class extras
+{
+    public static void echoOS() {
+        System.out.println("{{" + System.getProperty("os.name") + "}}");
+    }
+    public static void echoVersion() {
+        System.out.println("{{" + System.getProperty("os.name") + " " + System.getProperty("os.version") + "}}");
+    }
+    public static void echo(String arg) {
+        //code here
+        //System.out.println("The echo class worked!");
+        if(arg.toLowerCase().equals("$os")) {
+            //if we're looking for the OS property
+            echoOS();
+        } else if (arg.toLowerCase().equals("$version")) {
+            //if we're looking for the host OS version
+            echoVersion();
+        } else {
+            //otherwise just return the text
+            System.out.println(arg);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("jLinux - Copyright 2016, Brendan Manning");
+    }
+}
