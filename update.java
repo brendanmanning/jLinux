0f6fa184863676faa39a968acbbc0d22270cce5f
brendanmanning <BrendanManning19380@gmail.com>
2016-02-21 10:59:05 -0500

    Added files via upload

---
+++ update.java
@@ -0,0 +1,19 @@
+
+/**
+ * Update class for the update command
+ * can't actually do anything yet except for printing upgrade instaructions.
+ * 
+ * @author (Brendan Manning) 
+ * @version (jLinux 2.5)
+ */
+public class update
+{
+   public static void doUpdate() {
+       if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+           System.out.println("To update jLinux, run jLinuxUpdater.exe in you jLinux install folder");
+           System.out.println("*jLinux install folder is C:\\Program Files\\jLinux");
+       } else {
+           System.out.println("To update jLinux, download the latest JAR file from jLinux.net and replace your existing .jar with it");
+       }
+   }
+}
