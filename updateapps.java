b1f925518cfab131949d3600bd9f7fb225474815
brendanmanning <BrendanManning19380@gmail.com>
2016-02-21 10:59:15 -0500

    Added files via upload

---
+++ updateapps.java
@@ -0,0 +1,24 @@
+
+/**
+ * Updates every 3rd party app from the appstore by getting the newest version
+ * 
+ * @author (Brendan Manning) 
+ * @version (jLinux 2.5)
+ */
+
+import java.util.*;
+public class updateapps
+{
+   public static boolean doUpdate() {
+       System.out.println("This command is not finished yet!");
+       System.out.println("Future versions of jLinux wil have a working updateapps command");
+       /*
+        * List allInstalled = listapps.list();
+       return true;
+       for (int i = 0; i < allInstalled.size(); i++) {
+			main.deleteContents(allInstalled.get(i));
+		}
+        */
+       return true;
+   }
+}
